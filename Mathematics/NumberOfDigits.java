b3db8e43425a08096293dc1e7e0980a546545a10
viswamvs <viswavickymvs@gmail.com>
2021-04-10 19:02:59 +0530

    Counting digits in a number

---
+++ Mathematics/NumberOfDigits.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class NumberOfDigits {
+
+    public int countDigit(int n){
+        int count = 0;
+        while(n!=0){
+            n=n/10;
+            count++;
+        }
+        return count;
+    }
+
+    public int countDigitRecursive(int n){
+        if(n==0)
+            return 0;
+        return 1 + countDigitRecursive(n/10);
+    }
+
+    public int countDigitLog(int n){
+        return (int) Math.floor(Math.log10(n) + 1);
+    }
+}
