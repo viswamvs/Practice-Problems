d0267b6fda909153a9c3debb4dda015d04ac0210
viswamvs <viswavickymvs@gmail.com>
2021-04-11 17:29:05 +0530

    prime number

---
+++ Mathematics/PrimeNumber.java
@@ -0,0 +1,42 @@
+package com.company;
+
+public class PrimeNumber {
+
+    public boolean prime(int n){
+        if (n == 1)
+            return false;
+        for(int i=2;i<n;i++){
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    // efficient method
+    public boolean primeNumber(int n){
+        if(n == 1)
+            return false;
+        for (int i = 2; i*i <= n; i++)
+        {
+            if(n%i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    //more efficient method
+    public boolean primeEfficient(int n){
+        if (n == 1)
+            return false;
+        if (n == 2 || n == 3)
+            return true;
+        if (n%2 == 0 || n%3 == 0)
+            return false;
+        for(int i = 5; i*i <= n; i=i+6)
+        {
+            if (n % i == 0 || n%(i+2) == 0)
+                return false;
+        }
+        return true;
+    }
+}
