bbd8144ef4018e1109a31fbb305ef34cb82b662d
viswamvs <viswavickymvs@gmail.com>
2021-04-16 09:26:15 +0530

    quadratic equation using arraylist

--- Mathematics/QuadraticEquation.java
+++ Mathematics/QuadraticEquation.java
@@ -15,4 +15,29 @@ public class QuadraticEquation {
             System.out.print(Math.floor(x) + " " + Math.floor(y));
         }
     }
+
+    //using arraylist
+    public ArrayList<Integer> quadraticRoots(int a, int b, int c) {
+        // code here
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        long D = b*b -4*a*c;
+        if(D < 0)
+        {
+            System.out.print("Imaginary");
+        }
+        else if (D == 0)
+        {
+            double x = (-b + (float)Math.sqrt(D)) / (2.0*a);
+            numbers.add((int) Math.floor(x));
+            numbers.add((int) Math.floor(x));
+        }
+        else
+        {
+            int x = (int)Math.floor((-b + (float)Math.sqrt(D)) / (2.0*a));
+            int y = (int)Math.floor((-b - (float)Math.sqrt(D)) / (2.0*a));
+            numbers.add(Math.max(x,y));
+            numbers.add(Math.min(x,y));
+        }
+        return (numbers);
+    }
 }
