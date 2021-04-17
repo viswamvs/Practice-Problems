3bb575f6fbb4e492d237086c508c8888854b0e3e
viswamvs <viswavickymvs@gmail.com>
2021-04-17 05:31:01 +0530

    check whether kth bit is set or not

---
+++ Bit Manipulation/KthSet.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class KthSet {
+
+    //using left shift operator
+    public void kthbitL(int n, int k){
+        if ((n & (1 << (k - 1))) != 0)
+            System.out.print("SET");
+        else
+            System.out.print("NOT SET");
+        }
+
+    // using right shift operator
+    public void kthbitR(int n, int k) {
+        if((n >> (k-1) & 1 ) != 0)
+            System.out.println("SET");
+        else
+            System.out.println("NOT SET");
+    }
+}
