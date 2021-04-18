7fb5ad91d38212229a0f51718a0ae4167b97e24c
viswamvs <viswavickymvs@gmail.com>
2021-04-18 06:59:25 +0530

    check whether a number is power of two or not

---
+++ Bit Manipulation/PowerOfTwo.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class PowerOfTwo {
+
+    //naive solution
+    public boolean pow(int n)
+    {
+        if(n == 0)
+            return false;
+        while (n != 1)
+        {
+            if (n % 2 != 0)
+                return false;
+            n = n / 2;
+        }
+        return true;
+    }
+
+    //efficient solution
+    public boolean powOfTwo(int n)
+    {
+        if (n == 0)
+            return false;
+        return ((n & (n-1)) == 0);
+    }
+}
