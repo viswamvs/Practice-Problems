9482763b6e32ccf8c19ba21d57ba12fb6b275403
viswamvs <viswavickymvs@gmail.com>
2021-04-23 05:18:30 +0530

    delete operation in array

--- Array/SampleArray.java
+++ Array/SampleArray.java
@@ -26,4 +26,22 @@ public class SampleArray {
         arr[idx] = x;
         return (n+1);
     }
+
+    //delete operation
+    int delete(int arr[], int n, int x)
+    {
+        int i;
+        for (i=0;i<n;i++)
+        {
+            if (arr[i] == x)
+                break;;
+        }
+        if (i == n)
+            return n;
+        for (int j = i; j<n-1;j++)
+        {
+            arr[j] = arr[j+1];
+        }
+        return (n-1);
+    }
 }
