eb0b243093f1f625706686aefa20513a7080bfdc
viswamvs <viswavickymvs@gmail.com>
2021-06-04 07:17:12 +0530

    N-bonacci series of M elements

---
+++ Array/BonacciSeries.java
@@ -0,0 +1,22 @@
+public class BonnaciSeries {
+    
+    public static void bonacci(int n, int m)
+    {
+        int[] a = new int[m];
+        for(int i=0;i<n-1;i++)
+        {
+            a[i] = 0;
+        }
+        a[n-1] = 1;
+        a[n] = 1;
+        for(int i=n+1;i<m;i++)
+        {
+            a[i] = a[i-1]+a[i-1] - a[i-n-1];
+        }
+        for(int i=0;i<m;i++)
+            System.out.print(a[i] + " ");
+    }
+    public static void main(String[] args){
+        bonacci(4,10);
+    }
+}
