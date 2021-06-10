e775473603bad0c820e8e8c1128c96f5dcdf6fd3
viswamvs <viswavickymvs@gmail.com>
2021-06-10 17:50:19 +0530

    sum of digits

---
+++ Recursion/SumOfDigits.java
@@ -0,0 +1,12 @@
+public class SumOfDigits{
+    
+   public static int sumOfDigits(int n)
+   {
+        if(n==0)
+            return 0;
+        return sumOfDigits(n/10) + n % 10;
+   }
+    public static void main(String[] args){
+       System.out.println(sumOfDigits(10));
+    }
+}
