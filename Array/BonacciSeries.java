43824d10225d227ea55d8dc889a1857aeb77d9c5
viswamvs <viswavickymvs@gmail.com>
2021-06-11 20:21:44 +0530

    naive approach added

--- Array/BonacciSeries.java
+++ Array/BonacciSeries.java
@@ -1,5 +1,27 @@
 public class BonnaciSeries {
     
+    //naive solution
+    public static void bonacciNum(int n, int m)
+    {
+        int a[] = new int[m];
+        for(int i=0;i<n;i++)
+            a[i] = 0;
+        a[n-1] = 1;
+        a[n] = 1;
+        for(int i=n+1;i<m;i++)
+        {
+            for(int j=i-n;j<i;j++)
+            {
+                a[i] += a[j];
+            }
+        }
+        for(int i=0;i<m;i++)
+        {
+            System.out.print(a[i] + " ");
+        }
+    }
+    
+    //efiicient solution
     public static void bonacci(int n, int m)
     {
         int[] a = new int[m];
@@ -17,6 +39,6 @@ public class BonnaciSeries {
             System.out.print(a[i] + " ");
     }
     public static void main(String[] args){
-        bonacci(4,10);
+        bonacciNum(3,8);
     }
 }
