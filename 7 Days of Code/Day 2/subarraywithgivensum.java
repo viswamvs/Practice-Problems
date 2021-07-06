46b6a7e79b5291f31f41cde4b7bd25e1367a8ed6
viswamvs <viswavickymvs@gmail.com>
2021-07-06 07:22:05 +0530

    day 2 completed

---
+++ 7 Days of Code/Day 2/subarraywithgivensum.java
@@ -0,0 +1,61 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main{
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+            int s = sc.nextInt();
+
+            int[] m = new int[n];
+            for (int j = 0; j < n; j++) {
+                m[j] = sc.nextInt();
+            }
+            
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.subarraySum(m, n, s);
+            for(int ii = 0;ii<res.size();ii++)
+                System.out.print(res.get(ii) + " ");
+            System.out.println();
+        }
+    }
+
+}// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find a continuous sub-array which adds up to a given number.
+    static ArrayList<Integer> subarraySum(int[] arr, int n, int s) 
+    {
+        // Your code here
+        ArrayList<Integer> l = new ArrayList<Integer>();
+        int start = 0;
+        int cursum = 0;
+        for(int i=0;i<=n;i++)
+        {
+            while(cursum > s && start < i-1)
+            {
+                cursum -= arr[start];
+                start++;
+            }
+            if(cursum == s)
+            {
+                start++;
+                l.add(start);
+                l.add(i);
+                return l;
+            }
+            if(i < n)
+                cursum += arr[i];
+        }
+        l.add(-1);
+        return l;
+    }
+}
