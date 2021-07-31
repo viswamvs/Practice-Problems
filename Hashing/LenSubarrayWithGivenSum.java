5df9c792c2908e24554a619ca4c05161aa03fb5c
viswamvs <viswavickymvs@gmail.com>
2021-07-31 05:47:39 +0530

    added efficient approach

--- Hashing/LenSubarrayWithGivenSum.java
+++ Hashing/LenSubarrayWithGivenSum.java
@@ -1,3 +1,5 @@
+/*package whatever //do not write package name here */
+
 import java.io.*;
 import java.util.*;
 
@@ -21,8 +23,28 @@ class GFG {
         return res;
     }
     
+    //efficient solution
+    public static int lenSubArray(int[] arr, int sum)
+    {
+        int n = arr.length;
+        int pre_sum = 0;
+        int res = 0;
+        HashMap<Integer,Integer> m = new HashMap<Integer,Integer>();
+        for(int i=0;i<n;i++)
+        {
+            pre_sum += arr[i];
+            if(pre_sum == sum)
+                res = i+1;
+            if(m.containsKey(pre_sum) == false)
+                m.put(pre_sum, i);
+            if(m.containsKey(pre_sum - sum))
+                res = Math.max(res, i - m.get(pre_sum-sum));
+        }
+        return res;
+    }
+    
 	public static void main (String[] args) {
-		int[] a = {5,2,3,4};
-		System.out.println(maxLen(a,5));
+		int[] a = {10,5,2,7,1,9};
+		System.out.println(lenSubArray(a,15));
 	}
 }
