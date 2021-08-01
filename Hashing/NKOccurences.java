803d9f7dcdcc61a75d1d41516fe95ac055e67aa3
viswamvs <viswavickymvs@gmail.com>
2021-08-01 07:13:44 +0530

    added efficient method

--- Hashing/NKOccurences.java
+++ Hashing/NKOccurences.java
@@ -39,8 +39,46 @@ class GFG {
                 System.out.print(e.getKey()+" ");
     }
     
+    //more efficient solution
+    public static void nk(int arr[], int k)
+    {
+        HashMap<Integer,Integer> m = new HashMap<Integer,Integer>();
+        int n = arr.length;
+        for(int i=0;i<n;i++)
+        {
+            if(m.containsKey(arr[i]))
+            {
+                int count = m.get(arr[i]);
+                m.put(arr[i], count + 1);
+            }
+            else if(m.size() < k-1)
+                m.put(arr[i], 1);
+            else
+            {
+                for(Map.Entry e : m.entrySet())
+                {
+                    Integer c = (Integer) e.getValue();
+                    m.put((Integer) e.getKey(), c-1);
+                    if((Integer) e.getValue() == 0)
+                        m.remove(e.getKey());
+                }
+            }
+        }
+        for(Map.Entry x : m.entrySet())
+        {
+            int count = 0;
+            for(int i=0;i<n;i++)
+            {
+                if((Integer)x.getKey() == arr[i])
+                    count++;
+            }
+            if(count > n/k)
+                System.out.print(x.getKey() + " ");
+        }
+    }
+    
 	public static void main (String[] args) {
 		int[] a = {10,10,20,30,20,10,10};
-		nOccurences(a,3);
+		nk(a,3);
 	}
 }
